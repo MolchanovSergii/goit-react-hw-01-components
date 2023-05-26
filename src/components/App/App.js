bb0427919bfdc71c49594ab3e5531c37623d243b
MolchanovSergii <molchanse@gmail.com>
2023-05-26 17:26:41 +0300

    finish

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from '../Profile/user.json';
 import data from '../Statistics/data.json';
+import friends from '../FriendList/friends.json';
+import transactions from '../TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -14,8 +18,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map((item, idx) => (
+          <tr
+            key={item.id}
+            className={idx % 2 === 0 ? css.evenRow : css.oddRow}
+          >
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
